70abd0ea1515d16b2aab470023d4fc5acd720658
Octowl <alsaff1987@gmail.com>
2014-06-30 17:48:02 +0300

    Initial commit from local git.

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Aziz Alsaffar on 6/30/14.
+//  Copyright (c) 2014 Aziz Alsaffar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
