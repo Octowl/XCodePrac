60216271fc8a896e6b3aecb48888614d02470e5e
Octowl <alsaff1987@gmail.com>
2014-07-05 12:00:43 +0300

    Added ToDoListTableViewController subclass. Changed To Do List Scene to use it.

---
+++ ToDoList/ToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Aziz Alsaffar on 7/5/14.
+//  Copyright (c) 2014 Aziz Alsaffar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoListTableViewController : UITableViewController
+
+@end
