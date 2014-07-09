c2a1e207c41a30341007445d47100a01276642df
Octowl <alsaff1987@gmail.com>
2014-07-09 12:03:59 +0300

    Added ToDoItem class

---
+++ ToDoList/ToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.h
+//  ToDoList
+//
+//  Created by Aziz Alsaffar on 7/9/14.
+//  Copyright (c) 2014 Aziz Alsaffar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToDoItem : NSObject
+
+@end
